fc12198a6d7573124489e1d460aa110f3f85f15e
XXXFLOW <81309380+abdullah7701@users.noreply.github.com>
2025-05-11 15:16:51 +0500

    api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 export const fetchSensorData = async () => {
   try {
-    const response = await fetch('/get-sensor-data/');
+    const response = await fetch('http://161.35.107.143:8080/get-sensor-data/');
     const text = await response.text();
     const sanitizedText = text.replace(/Infinity/g, 'null');
     const result = JSON.parse(sanitizedText);
